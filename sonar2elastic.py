cf0440e7609b037a7358b184e24e50a7725b8f33
Silvio Mendes Pedrosa <nirvana.brz@gmail.com>
2020-02-06 11:29:03 -0300

    Update sonar2elastic.py

--- sonar2elastic.py
+++ sonar2elastic.py
@@ -1,22 +1,22 @@
 """Sonar to Elasticsearch v0.1.
 
 Usage:
-  sonar2elastic.py --sonarApiUrl="http://10.106.248.22:30310/sonar/api" --elasticApiUrl="logs-sul.mapfre.net" --elasticApiPort=80 --idReg=26 --indexName="sonarqube" --pipeline="Tronweb/Tronweb_build" --pipelineDescription="/PY/Newtron/Certificacion" --sonarProjectKey="com.mapfre.tronweb:twserver"
+  sonar2elastic.py --sonarApiUrl="http://10.0.0.1:30310/sonar/api" --elasticApiUrl="10.0.0.2" --elasticApiPort=9000 --idReg=26 --indexName="sonarqube" --pipeline="MyJob" --pipelineDescription="MyJobData" --sonarProjectKey="br.com.teste.app:server" 
   sonar2elastic.py -h
   sonar2elastic.py --version
 
 Options:
   -h                                                        Exibe esta ajuda.
-  --version                                                 Exibe a versao
+  --version                                                 Exibe a versão
   --sonarApiUrl=<sonarApiUrl>                   <Stgring>   Url da API do SonarQube.
   --elasticApiUrl=<elasticApiUrl>               <String>    Url da API do Elasticsearch.
   --elasticApiPort=<elasticApiPort>             <Integer>   Porta da API do Elasticsearch.
   --idReg=<idReg>                               <Integer>   Id do registro que sera inserido no Elasticsearch, sugiro usar o numero do build do Jenkins.
   --indexName=<indexName>                       <String>    Nome do indice no Elastisearch: Ex.: "sonarqube".
-  --pipeline=<pipeline>                         <String>    Nome completo da pipeline do Jenkins: Ex.: "Tronweb/Tronweb_build".
-  --pipelineDescription=<pipelineDescription>   <String>    String contendo dados extras para graficos do Kiband: Ex.: "/PY/Newtron/Certificacion".
-  --sonarProjectKey=<sonarProjectKey>           <string>    Key do projeto dentro do Sonar: Ex.: "com.mapfre.tronweb:twserver".
-"""
+  --pipeline=<pipeline>                         <String>    Nome completo da pipeline do Jenkins: Ex.: "MyJob".
+  --pipelineDescription=<pipelineDescription>   <String>    String contendo dados extras para graficos do Kiband: Ex.: "MyJobData".
+  --sonarProjectKey=<sonarProjectKey>           <string>    Key do projeto dentro do Sonar: Ex.: "br.com.teste.app:server"."""
+
 import pip
 
 def errorMsg(stage, error):
